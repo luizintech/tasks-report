641e413148951f11ba24bacab960a9b3d5c65575
André Figueiredo <figueiredoaphilippe@gmail.com>
2022-12-26 18:32:56 +0000

    feat(sidenav/header): add the sidenav and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LayoutsModule } from './components/layouts/layouts.module';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/pages/pages.menu.ts
@@ -0,0 +1,41 @@
+import { NavItem } from './../models/NavItem';
+
+export const menu: NavItem[] = [
+  {
+    displayName: 'Reports',
+    iconName: 'fas fa-clock',
+    route: 'reports',
+  },
+  {
+    displayName: 'Menu 2',
+    iconName: 'fas fa-clock',
+    route: 'entradasGADE',
+  },
+  {
+    displayName: 'Menu 3',
+    iconName: 'fas fa-clock',
+    children: [
+      {
+        displayName: 'Menu 3.1',
+        iconName: 'fas fa-clock',
+        route: '/misexpedientes',
+      },
+      {
+        displayName: 'Menu 3.2',
+        iconName: 'fas fa-clock',
+        route: '/todos',
+      },
+    ],
+  },
+  {
+    displayName: 'Menu 4',
+    iconName: 'fas fa-clock',
+    children: [
+      {
+        displayName: 'Menu 4.1',
+        iconName: 'fas fa-clock',
+        route: '/busquedaperfiles',
+      },
+    ],
+  },
+];

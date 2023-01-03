efe5dd6a3b3749e3156dc82b33058271972b005e
Luiz Godoy <lgodoy@alter-solutions.com>
2023-01-03 10:06:15 +0000

    criada a  migration e tabela para a tabela UserInfo

--- api-netcore/TasksReportManager/TasksReportManager.EFPersistence/Migrations/20230103100506_CreateTableUserInfo.Designer.cs
+++ api-netcore/TasksReportManager/TasksReportManager.EFPersistence/Migrations/20230103100506_CreateTableUserInfo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TasksReportManager.EFPersistence.DbContexts;
 
@@ -11,9 +12,11 @@ using TasksReportManager.EFPersistence.DbContexts;
 namespace TasksReportManager.EFPersistence.Migrations
 {
     [DbContext(typeof(TaskReportDbContext))]
-    partial class TaskReportDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230103100506_CreateTableUserInfo")]
+    partial class CreateTableUserInfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

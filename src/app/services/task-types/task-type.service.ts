2721e4806848151ecf57b1fdf7681fc56e638680
Luiz Godoy <lgodoy@alter-solutions.com>
2023-01-09 14:54:04 +0000

    escrita do serviço TaskType

--- src/app/services/task-types/task-type.service.ts
+++ src/app/services/task-types/task-type.service.ts
@@ -1,9 +1,68 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Result } from 'src/app/dtos/Result';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskTypeService {
 
-  constructor() { }
+  private resourceName = "TaskType";
+
+  constructor(private http: HttpClient) { }
+
+  public listAll(): Observable<TaskType[]> {
+    let headers = new HttpHeaders({
+      'content-Type': 'application/json',
+      'Access-Control-Allow-Origin': window.location.host
+    });
+
+    return this.http.get<TaskType[]>(`${environment.taskManagerApi}/${this.resourceName}`, 
+      { headers: headers });
+  }
+
+  public getById(id: number): Observable<TaskType[]> {
+    let headers = new HttpHeaders({
+      'content-Type': 'application/json',
+      'Access-Control-Allow-Origin': window.location.host
+    });
+
+    return this.http.get<TaskType[]>(`${environment.taskManagerApi}/${this.resourceName}/${id}`, 
+      { headers: headers });
+  }
+
+  public post(taskType: TaskType): Observable<Result> {
+    let headers = new HttpHeaders({
+      'content-Type': 'application/json',
+      'Access-Control-Allow-Origin': window.location.host
+    });
+
+    return this.http.post<Result>(`${environment.taskManagerApi}/${this.resourceName}`, 
+      taskType,
+      { headers: headers });
+  }
+
+  public put(id: number, taskType: TaskType): Observable<Result> {
+    let headers = new HttpHeaders({
+      'content-Type': 'application/json',
+      'Access-Control-Allow-Origin': window.location.host
+    });
+
+    return this.http.put<Result>(`${environment.taskManagerApi}/${this.resourceName}/${id}`, 
+      taskType,
+      { headers: headers });
+  }
+
+  public delete(id: number): Observable<Result> {
+    let headers = new HttpHeaders({
+      'content-Type': 'application/json',
+      'Access-Control-Allow-Origin': window.location.host
+    });
+
+    return this.http.put<Result>(`${environment.taskManagerApi}/${this.resourceName}/${id}`, 
+      { headers: headers });
+  }
+
 }
